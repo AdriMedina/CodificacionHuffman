67007798024f88b586f1640854cce2864b29a75c
AdriMedina <adri.mdn90@gmail.com>
2016-06-07 16:00:43 +0200

    Creación del proyecto
    Creación del proyecto vacio con la herramienta IntelliJ Idea. Añadidas
    las clases base del árbol de codificación

---
+++ PracticaFinal/src/Nodo.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by Adri Medina on 07/06/2016.
+  */
+
+/**
+  * Clase abstracta para representar los nodos del árbol de codificación
+  */
+abstract class Nodo {
+
+}
---
+++ PracticaFinal/src/NodoIntermedio.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by Adri Medina on 07/06/2016.
+  */
+
+/**
+  * Clase para representar los nodos no terminales (intermedios) del árbol de codificación
+  */
+class NodoIntermedio extends Nodo{
+
+}
